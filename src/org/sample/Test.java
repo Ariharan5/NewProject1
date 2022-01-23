39b31db1ba163e2bf55c764ea3a71a5641f13778
Ariharan5 <ariharan.s143@gmail.com>
2022-01-23 17:02:49 +0530

    Some Project added

---
+++ src/org/sample/Test.java
@@ -0,0 +1,22 @@
+package org.sample;
+
+public class Test {
+	public void test1() {
+		System.out.println("This is test case : 1");
+	}
+
+	public void test2() {
+		System.out.println("This is test case : 2");
+	}
+
+	public void test3() {
+		System.out.println("This is test case : 3");
+	}
+
+	public static void main(String[] args) {
+		Test s = new Test();
+		s.test1();
+		s.test2();
+		s.test3();
+	}
+}
